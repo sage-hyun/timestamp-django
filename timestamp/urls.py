fe7f90e982c1ba233bda9fc0c9d5cd92d7af645d
sage-hyun <dan06052@naver.com>
2022-03-20 00:39:02 +0900

    t: Design API structure for creating timestamps

--- timestamp/urls.py
+++ timestamp/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('book/<int:book_id>/', views.book, name='book'),
     path('song/<int:song_id>/', views.song, name='song'),
+    path('bookmark/', views.bookmark, name='bookmark'),
+    path('favorite/', views.favorite, name='favorite'),
+    path('toc/', views.memo, name='toc'),
+    path('memo/', views.memo, name='memo'),
+    path('marker/', views.marker, name='marker'),
 ]
--- timestamp/views.py
+++ timestamp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Audio, Timestamp
 
 # Create your views here.
@@ -12,6 +13,7 @@ def index(request):
 def book(request, book_id):
     book = get_object_or_404(Audio, pk=book_id)
     context = {
+        'audio_id': book_id,
         'book_title': book.title,
         'book_filepath': book.filepath,
         'timestamps': Timestamp.objects.filter(audio_id=book_id)
@@ -21,8 +23,24 @@ def book(request, book_id):
 def song(request, song_id):
     song = get_object_or_404(Audio, pk=song_id)
     context = {
+        'audio_id': song_id,
         'song_title': song.title,
         'song_filepath': song.filepath,
         'timestamps': Timestamp.objects.filter(audio_id=song_id)
     }
     return render(request, 'song/index.html', context)
+
+def bookmark(request):
+    pass
+
+def favorite(request):
+    pass
+
+def toc(request):
+    pass
+
+def memo(request):
+    pass
+
+def marker(request):
+    pass
