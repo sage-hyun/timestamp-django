fa5ee9cac294a314a37b312e2689032710c4517e
sage-hyun <dan06052@naver.com>
2022-03-19 17:08:26 +0900

    Feat: Read data from DB (SQLite)

--- timestamp/views.py
+++ timestamp/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Audio, Timestamp
 
 # Create your views here.
 def index(request):
-    context = {'book_list': [
-        {'id':1, 'title':'Life Without Limits'},
-        {'id':2, 'title':'Unashamed'},
-    ]
+    context = {
+        'book_list': Audio.objects.filter(media_type='BOOK'),
+        'song_list': Audio.objects.filter(media_type='SONG'),
     }
     return render(request, 'index.html', context)
 
 def book(request, book_id):
+    book = get_object_or_404(Audio, pk=book_id)
     context = {
-        'book_title': "Life Without Limits",
-        'book_filepath': "http://surething.synology.me/audiobook_timestamp/audio/Life_Without_Limits.mp3"
-        }
+        'book_title': book.title,
+        'book_filepath': book.filepath,
+        'timestamps': Timestamp.objects.filter(audio_id=book_id)
+    }
     return render(request, 'book/index.html', context)
 
 def song(request, song_id):
+    song = get_object_or_404(Audio, pk=song_id)
     context = {
-        'song_title': "Known, Seen, Loved",
-        'song_filepath': ""
-        }
+        'song_title': song.title,
+        'song_filepath': song.filepath,
+        'timestamps': Timestamp.objects.filter(audio_id=song_id)
+    }
     return render(request, 'song/index.html', context)
