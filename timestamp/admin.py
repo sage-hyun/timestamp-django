dde88ec99a1fda6ad36245a3c726dd816240ea7f
sage-hyun <dan06052@naver.com>
2022-03-19 17:07:35 +0900

    Feat: Make DB modifiable in the admin

--- timestamp/admin.py
+++ timestamp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Audio, Timestamp, Memo
+
+admin.site.register(Audio)
+admin.site.register(Timestamp)
+admin.site.register(Memo)
