18bb262de2a243be8c30271a8f9ad7b5c801c6c8
sage-hyun <dan06052@naver.com>
2022-03-30 17:18:02 +0900

    Feat: show marker timeline

--- timestamp/static/scripts/song/main.js
+++ timestamp/static/scripts/song/main.js
@@ -1,7 +1,31 @@
 import { create_timestamp } from "../timestamp-api-controller.js";
 
 const add_marker = document.getElementById("marker-add-btn");
+const marker_timeline = document.getElementById("marker-timeline");
+const progress_bar = document.getElementById("progress-bar");
+
+let timeline_scale = 20;
+let marker_width = 16;
+
+myAudio.addEventListener("loadeddata", () => {
+    progress_bar.style.width = `${myAudio.duration * timeline_scale}px`;
+    marker_timeline.style.width = `${(myAudio.duration) * timeline_scale - marker_width}px`;
+
+    const li_all = document.querySelectorAll("li");
+    li_all.forEach(li => {
+        add_timeline_marker(li.getAttribute("data-second"));
+    });
+})
+
+function add_timeline_marker(sec) {
+    var div_marker = document.createElement("div");
+    div_marker.innerHTML = "△";
+    div_marker.style.position = "absolute";
+    div_marker.style.left = `${(sec / myAudio.duration) * 100}%`;
+    marker_timeline.appendChild(div_marker);
+}
 
 add_marker.onclick = () => {
     create_timestamp("marker");
+    add_timeline_marker(myAudio.currentTime);
 }
